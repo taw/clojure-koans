6cfc74afa012f83210855df39bec53b3dfaa9bac
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-11-19 23:35:42 +0000

    Koan 14

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,32 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [todo coll  acc '()]
+    (if (empty? todo)
+      acc
+      (recur (rest todo) (cons (first todo) acc))
+    )
+  )
+)
 
 (defn factorial [n]
-  __)
+  (loop [n   n
+         acc 1]
+  (if (= n 1)
+    acc
+    (recur (- n 1) (* n acc))
+)))
 
 (meditations
   "Recursion ends with a base case"
